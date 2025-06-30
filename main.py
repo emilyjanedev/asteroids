9f93e1a0e6547fb69572d6b67883fe9697ac3c9c
Emily Schur <ejschur@gmail.com>
2025-06-30 13:56:00 -0700

    Restrict game to draw max 60fps

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from constants import *
 def main():
     pygame.init()
     print("Starting Asteroids!")
+
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +18,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0,0))
         pygame.display.flip()
+        dt = game_clock.tick(60)
 
 if __name__ == "__main__":
     main()
